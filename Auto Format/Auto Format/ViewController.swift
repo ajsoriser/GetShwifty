20620f5988abd2143d59748242d3824c8246f33f
Aaron James <soriser@Aarons-Mac.local>
2017-12-08 09:20:17 -0800

    Homework For 12/8/17

---
+++ Auto Format/Auto Format/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Auto Format
+//
+//  Created by Aaron James on 12/8/17.
+//  Copyright © 2017 Aaron James. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+
+}
+
